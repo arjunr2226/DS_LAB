64d6fa89c46f1539a7110018478b6b5e66d268b4
arjun <2226@tkmce.ac.in>
2023-02-21 10:33:10 +0530

    added more feature

--- circular_linkedlist.c
+++ circular_linkedlist.c
@@ -7,147 +7,161 @@ struct node
 	struct node *next;
 }*tail=NULL;
 
-
-//Display
-
-void display()
-{
-	struct node *temp;
-	temp = tail->next;
-	if(tail == NULL)
-		printf("UNDERFLOW\n");
-	else
-	{
-		do
-		{
-			printf("%d ", temp->data);
-			temp = temp->next;
-		}while(temp != tail->next);
-	}
-	printf("\n");
-}
-
-//Inserting at beg
-
 void insert_beg()
-{
-	struct node *newnode, *temp;
-	temp = tail;
-	newnode = (struct node*)malloc(sizeof(struct node));
-	newnode->next = NULL;
-	printf("ENTER THE ELEMENT :");
-	scanf("%d", &newnode->data);
-	if(tail == NULL)
-	{
-		tail = newnode;
-		tail->next = newnode;
-	}
-	else
-	{
-		newnode->next = tail->next;
-		tail->next = newnode;
-	}
+ {
+ 	struct node *newnode;
+ 	newnode = (struct node*)malloc(sizeof(struct node));
+ 	newnode->next = NULL;
+ 	printf("ENTER THE DATA: ");
+ 	scanf("%d", &newnode->data);
+ 	if(tail == NULL)
+ 	{
+ 		tail = newnode;
+ 		tail->next = tail;
+ 	}
+ 	else
+ 	{
+ 		newnode->next = tail->next;
+ 		tail->next = newnode;
+ 	}
 }
-
-//Inserting at mid
 
 void insert_end()
 {
 	struct node *newnode;
 	newnode = (struct node*)malloc(sizeof(struct node));
 	newnode->next = NULL;
-	printf("ENTER THE ELEMENT :");
+	printf("ENTER THE DATA: ");
 	scanf("%d", &newnode->data);
 	if(tail == NULL)
 	{
 		tail = newnode;
-		tail->next = newnode;
+		tail->next = tail;
 	}
 	else
 	{
 		newnode->next = tail->next;
 		tail->next = newnode;
-		tail = tail->next;
+		tail = newnode;
 	}
 }
 
-//Inserting at mid
-
-int insert_mid()
+void insert_mid()
 {
-	struct node *newnode, *temp = tail->next;
+	struct node *newnode, *temp=tail->next;
 	int pos;
 	newnode = (struct node*)malloc(sizeof(struct node));
 	newnode->next = NULL;
-	printf("ENTER POSITION TO INSERT AFTER :");
+	printf("ENTER THE POSITION: ");
 	scanf("%d", &pos);
-	printf("ENTER THE ELEMENT :");
+	printf("ENTER DATA: ");
 	scanf("%d", &newnode->data);
 	while(pos > 1)
 	{
 		temp = temp->next;
 		pos--;
 	}
+	
 	newnode->next = temp->next;
 	temp->next = newnode;
+	
 	if(temp == tail)
 	{
-		tail = tail->next;
+		tail = newnode;
 	}
+	
 }
-
-//Deleting at beg
 
 void del_beg()
 {
-	struct node *temp;
-	temp = tail->next;
 	if(tail == NULL)
-		printf("UNDERFLOW\n");
+		printf("EMPTY\n");
+	else if(tail == tail->next)
+	{	
+		free(tail);
+		tail=NULL;
+	}
 	else
 	{
-		
+		struct node *temp=tail->next;
 		tail->next = temp->next;
 		free(temp);
 	}
 }
 
-//Deleting at end
-
 void del_end()
 {
-	struct node *temp, *head;
-	temp = tail->next;
 	if(tail == NULL)
-		printf("UNDERFLOW\n");
+		printf("EMPTY\n");
+	else if(tail == tail->next)
+	{
+		free(tail);
+		tail = NULL;
+	}
 	else
 	{
+		struct node *temp=tail->next;
 		while(temp->next != tail)
-		{
 			temp = temp->next;
-		}
 		temp->next = tail->next;
 		free(tail);
 		tail = temp;
 	}
 }
 
-//Deleting at mid
-
-int del_mid()
+void del_mid()
 {
-	struct node *temp = tail->next, *temp2;
-	int pos;
-	printf("ENTER POSITION TO DELETE AFTER :");
-	scanf("%d", &pos);
-	while(pos > 2)
+	if(tail == NULL)
+		printf("EMPTY\n");
+	else if(tail == tail->next)
 	{
-		temp = temp->next;
-		pos--;
+		printf("DELETING LAST NODE..\n");
+		free(tail);
+		tail = NULL;
 	}
-	temp2 = temp->next;
-	temp->next = temp2->next;
-	free(temp2);
+	else
+	{
+		struct node *temp=tail->next, *temp1;
+		int pos;
+		printf("ENTER THE POSITION: ");
+		scanf("%d", &pos);
+		while(pos > 2)
+		{
+			temp = temp->next;
+			pos--;
+		}
+		temp1 = temp->next;
+		temp->next = temp1->next;
+		if(temp1 == tail)
+		{
+			tail = temp;
+		}
+		free(temp1);
+	}
+}
+
+void display()
+{
+ 	if(tail == NULL)
+ 		printf("EMPTY\n");
+ 	else{
+ 		struct node *temp=tail->next;
+ 		do{
+	 		printf("%d ", temp->data);
+	 		temp = temp->next;
+ 		}while(temp != tail->next);
+ 	}	
+ 	printf("\n");
+}
+
+void create()
+{
+	int n, i;
+	printf("HOW MANY NODES: ");
+	scanf("%d", &n);
+	for(i=0; i<n; i++)
+			insert_end();
+	printf("\n");
 }
 
 void main()
@@ -156,7 +170,7 @@ void main()
 	do
 	{
 		printf("\n");
-		printf("ENTER \n0:EXIT\n1:DISPLAY\n2:INSERT_BEG\n3:INSERT_END\n4:INSERT_MID\n5:DEL_BEG\n6:DEL_END\n7:DEL_MID\nCHOICE:");
+		printf("ENTER \n0:EXIT\n1:DISPLAY\n2:INSERT_BEG\n3:INSERT_END\n4:INSERT_MID\n5:DEL_BEG\n6:DEL_END\n7:DEL_MID\n8:CREATE\nCHOICE:");
 		scanf("%d", &choice);
 		switch(choice)
 		{
@@ -187,6 +201,10 @@ void main()
 			case 7:
 				printf("\n");
 				del_mid();
+				break;
+			case 8:
+				printf("\n");
+				create();
 				break;											
 			case 0:
 				break;
